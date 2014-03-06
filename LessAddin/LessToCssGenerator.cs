843ab065e2699ca3ef40af745c058054b55bad84
Boris Scheiman <bscheiman@gmail.com>
2014-03-06 17:10:00 -0600

    Initial import

---
+++ LessAddin/LessToCssGenerator.cs
@@ -0,0 +1,20 @@
+﻿using MonoDevelop.Ide.CustomTools;
+using MonoDevelop.Core;
+using MonoDevelop.Projects;
+using System.IO;
+using dotless.Core;
+
+namespace LessAddin {
+	public class LessToCssGenerator : ISingleFileCustomTool {
+		public IAsyncOperation Generate(IProgressMonitor monitor, ProjectFile file, SingleFileCustomToolResult result) {
+			return new ThreadAsyncOperation(() => {
+				var dest = file.FilePath.ChangeExtension(".css");
+				var src = File.ReadAllText(file.FilePath);
+
+				result.GeneratedFilePath = dest;
+				File.WriteAllText(dest, Less.Parse(src));
+			}, result);
+		}
+	}
+}
+
